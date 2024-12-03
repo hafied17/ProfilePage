b6b7b4f13f09b71e4bbc4ebcfbdd588336d88b3f
hafied <hafied.shiddiqi@ist.id>
2024-12-03 17:09:21 +0700

    Initial Commit

---
+++ ProfilePage/ProfilePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfilePageApp.swift
+//  ProfilePage
+//
+//  Created by hafied on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ProfilePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
